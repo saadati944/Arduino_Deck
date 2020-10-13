e7c3fea2914c186376399671c8ec8d4b567d4721
saadati944 <saadati944@gmail.com>
2020-10-13 16:23:27 +0330

    load default profile. use user defined port in defprofile if avaliable.

--- deck.py
+++ deck.py
@@ -1,6 +1,20 @@
 import serial
+import profile
 
-sr=serial.Serial(input('enter port name : '),9600)
+# load default profile
+dpr=open('.\\defprofile','r')
+defprof=dpr.readline()
+lines=profile.load('.\\profiles\\'+defprof)
+print('loaded profile :',defprof)
+dpr.close()
+
+#use user defined port if avaliable else get port name from user.
+if 'port' in lines:
+    port=lines['port']
+else:
+    port=input('enter port name : ')
+
+sr=serial.Serial(port,9600)
 
 while True:
     print(sr.read())
